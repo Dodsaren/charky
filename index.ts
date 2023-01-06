e599e931287d4c997e17e05785c672c3c5e3b94c
Dodsaren <jimnils@hotmail.com>
2023-01-06 11:49:05 +0100

    Test

--- index.ts
+++ index.ts
@@ -2,6 +2,9 @@ import dotenv from 'dotenv'
 dotenv.config()
 import setupCronjobs from './src/cronJobs'
 import initBot from './src/bot'
+import logger from './src/logger'
+
+logger('APP LOADED WITH ENV:', process.env)
 
 function main() {
   setupCronjobs()
